f698bf24254856274938de5f9b9b59efb1c12555
Meder Kamalov <meder@kamalov.net>
2023-12-04 17:19:02 +0100

    Add credentials for websockets

--- src/main/java/com/fal/falclient/Realtime.kt
+++ src/main/java/com/fal/falclient/Realtime.kt
@@ -1,11 +1,44 @@
 import com.google.gson.Gson
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
-import okhttp3.HttpUrl
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
+
+const val TOKEN_EXPIRATION_SECONDS = 120
+val client = OkHttpClient()
+val gson = Gson()
+
+fun getToken(app: String, authKey: String): String {
+    val appAlias = app.split('-').drop(1).joinToString("-")
+    val jsonType = "application/json; charset=utf-8".toMediaType()
+    val bodyData = mapOf(
+        "allowed_apps" to listOf(appAlias),
+        "token_expiration" to TOKEN_EXPIRATION_SECONDS
+    )
+    val requestBody: RequestBody = gson.toJson(bodyData).toRequestBody(jsonType)
+
+    val request = Request.Builder()
+        .url("https://rest.alpha.fal.ai/tokens/")
+        .post(requestBody)
+        .addHeader("Authorization", "Key $authKey")
+        .build()
+
+    client.newCall(request).execute().use { response ->
+        val responseBody = response.body?.string() ?: ""
+        // Assuming the token is returned directly in the response. Adjust based on the actual API response.
+        return if (response.isSuccessful && responseBody.isNotBlank()) {
+            responseBody
+        } else {
+            throw Exception("Failed to get token: ${response.message}")
+        }
+    }
+}
+
 
 abstract class RealtimeConnection<Input>(private val sendFunction: (Input) -> Unit, private val closeFunction: () -> Unit) {
     fun send(input: Input) = sendFunction(input)
@@ -23,7 +56,8 @@ class UntypedRealtimeConnection(send: (String) -> Unit, close: () -> Unit) : Rea
 
 fun buildRealtimeUrl(app: String, host: String, token: String?): String {
     val protocol = if (token != null) "wss" else "ws"
-    return "$protocol://$app.$host/ws${token?.let { "?fal_jwt_token=$it" } ?: ""}"
+    val cleanToken = token?.trim('"')
+    return "$protocol://$app.$host/ws${cleanToken?.let { "?fal_jwt_token=$it" } ?: ""}"
 }
 
 class WebSocketConnection(
@@ -33,11 +67,24 @@ class WebSocketConnection(
 ) : WebSocketListener() {
     private val client = OkHttpClient()
     private var webSocket: WebSocket? = null
+    private var token: String? = null
+    private var job: Job? = null
 
-    fun connect(token: String?) {
-        val url = buildRealtimeUrl(app, "gateway.alpha.fal.ai", token)
-        val request = Request.Builder().url(url).build()
-        webSocket = client.newWebSocket(request, this)
+    suspend fun connect(authKey: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            token = getToken(app, authKey)
+            val url = buildRealtimeUrl(app, "gateway.alpha.fal.ai", token)
+            val request = Request.Builder().url(url).build()
+            webSocket = client.newWebSocket(request, this@WebSocketConnection)
+
+            // Refresh token periodically
+            job = launch {
+                while (isActive) {
+                    delay(TOKEN_EXPIRATION_SECONDS * 900L)
+                    token = getToken(app, authKey)
+                }
+            }
+        }
     }
 
     override fun onMessage(webSocket: WebSocket, text: String) {
@@ -54,6 +101,10 @@ class WebSocketConnection(
 
     fun close() {
         webSocket?.close(1000, "Programmatically closed")
+    }
+
+    fun isConnected(): Boolean {
+        return webSocket != null
     }
 }
 
