edcbccf37c1ae480aa72cc4bf4ab06b93fcb8dcb
Daniel Rochetti <daniel.rochetti@gmail.com>
2024-09-04 09:01:29 -0700

    chore: update package metadata

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 subprojects {
     group = "ai.fal.client"
-    version = "0.7.0-SNAPSHOT"
+    version = "0.7.0"
 
     apply(plugin = "com.diffplug.spotless")
     apply(plugin = "com.vanniktech.maven.publish")
@@ -27,14 +27,25 @@ subprojects {
     }
 
     mavenPublishing {
-        publishToMavenCentral(SonatypeHost.S01, automaticRelease = true)
-//        signAllPublications()
+        publishToMavenCentral(SonatypeHost.DEFAULT, automaticRelease = true)
+        signAllPublications()
         pom {
+            name.set("Fal Client Library")
+            description.set("A Client library for fal.ai APIs")
+            inceptionYear.set("2024")
             url.set("https://github.com/fal-ai/fal-java")
             licenses {
                 license {
                     name.set("MIT")
                     url.set("https://opensource.org/license/mit")
+                }
+            }
+            developers {
+                developer {
+                    id.set("fal")
+                    name.set("Fal AI")
+                    email.set("developers@fal.ai")
+                    url.set("https://github.com/fal-ai")
                 }
             }
             scm {
